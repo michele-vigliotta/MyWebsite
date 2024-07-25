c55bab0b84fcf5eca9e3c32ae20daeaccdd484e8
michele-vigliotta <michele.vigliotta00@gmail.com>
2024-07-25 23:30:59 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,49 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+let player;
+let cursors;
+
+function preload() {
+    this.load.image('frog', 'assets/frog.png')
+}
+
+function create() {
+    player = this.physics.add.sprite(400, 300, 'frog');
+    player.setScale(0.044);
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    player.setVelocity(0);
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+    }
+}
